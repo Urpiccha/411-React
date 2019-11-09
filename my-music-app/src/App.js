970bfa6b63c272eb1013f9252479fc8f0e394ceb
Urpiccha <ur.picha.hartman@gmail.com>
2019-11-09 13:50:32 -0600

    Something

--- my-music-app/src/App.js
+++ my-music-app/src/App.js
@@ -7,7 +7,7 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 // import Dashboard from "./components/Dashboard";
-import SignIn from "./components/SignIn";
+// import SignIn from "./components/SignIn";
 
 const useStyles = makeStyles(theme => ({
 	root: {
