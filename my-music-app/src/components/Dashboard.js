9d21703931ea2f08ec75357c8479f7066867e282
grantkee <grant.d.kee@gmail.com>
2019-11-09 12:41:47 -0600

    added props to dashboard class component

--- my-music-app/src/components/Dashboard.js
+++ my-music-app/src/components/Dashboard.js
@@ -4,14 +4,17 @@ import MasterVolumeCard from "./MasterVolumeCard";
 import SoundQuality from "./Card3-Sound Quality";
 
 export default class Dashboard extends Component {
-	state = {
-		value: 20,
-		online: true,
-		quality: "Normal",
-		user: "user"
+	constructor(props){
+		super(props);
+		this.state = {
+			value: 20,
+			online: true,
+			quality: "Normal",
+			user: "user"	
+		}
 	};
 
-	
+
 
 	render(){
 		
