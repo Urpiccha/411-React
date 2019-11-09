682ed4bb479649b5496649599df73bcfffa27796
Urpiccha <ur.picha.hartman@gmail.com>
2019-11-09 15:49:08 -0600

    More Stuff

--- my-music-app/src/components/Dashboard.js
+++ my-music-app/src/components/Dashboard.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import NavBar from "./NavBar";
 import MasterVolumeCard from "./MasterVolumeCard";
 import SoundQuality from "./Card3-Sound Quality";
+import OnlineMode from "./OnlineMode";
+// import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		height: 140,
+		width: 100
+	},
+	control: {
+		padding: theme.spacing(2)
+	}
+}));
 
 export default class Dashboard extends Component {
 	constructor(props) {
@@ -20,6 +36,7 @@ export default class Dashboard extends Component {
 				<NavBar />
 				<MasterVolumeCard />
 				<SoundQuality />
+				<OnlineMode />
 			</div>
 		);
 	}
--- my-music-app/src/components/OnlineMode.js
+++ my-music-app/src/components/OnlineMode.js
@@ -1,42 +1,34 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-// import SignIn from "./SignIn";
+import ReactDOM from "react-dom";
+import Switch from "react-switch";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles({
-	card: {
-		minWidth: 275
-	},
-	bullet: {
-		display: "inline-block",
-		margin: "0 2px",
-		transform: "scale(0.8)"
-	},
-	title: {
-		fontSize: 14
-	},
-	pos: {
-		marginBottom: 12
+class OnlineMode extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			checked: false
+		};
+		this.handleChange = this.handleChange.bind(this);
 	}
-});
-
-function OnlineMode() {
-	const classes = useStyles();
-
-	return (
-		<Card className={classes.card}>
-			<CardContent>
-				<h1>Switch to Offline Mode</h1>
-			</CardContent>
-			<CardActions>
-				<Button size="small">Online Mode</Button>
-			</CardActions>
-		</Card>
-	);
+	handleChange(checked) {
+		this.setState({ checked });
+	}
+	render() {
+		return (
+			<div>
+				<h2>Online/Offline Mode</h2>
+				<Switch
+					className="online-switch"
+					onChange={this.handleChange}
+					checked={this.state.checked}
+				/>
+				<p>
+					You are <b>{this.state.checked ? "Online" : "Offline"}</b>
+				</p>
+			</div>
+		);
+	}
 }
 
 export default OnlineMode;
